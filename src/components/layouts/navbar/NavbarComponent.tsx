6d46a3b6ae461c9572af3d09c80cabedb692ffa1
JessicaaSun <sunjessica05@gmail.com>
2024-03-31 19:15:25 +0700

    add responsive navbar

--- src/components/layouts/navbar/NavbarComponent.tsx
+++ src/components/layouts/navbar/NavbarComponent.tsx
@@ -6,50 +6,98 @@ import {
   NavbarContent,
   NavbarItem,
   Button,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
 } from "@nextui-org/react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { navbarItem } from "./menu";
 
-
-export default function NavbarComponent() {
+export default function App() {
   const pathname = usePathname();
-  // if (pathname.includes("/dashboard")) return null;
-  // else
-    return (
-      <Navbar className="bg-blue-200">
+
+  return (
+    <Navbar className="bg-blue-200 " disableAnimation>
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarMenuToggle />
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden pr-3" justify="start">
         <NavbarBrand>
-          <Image width={40} className="mr-4 rounded-full" height={40} src={"/assets/pic.jpg"} alt={""} />
-          <p className="font-bold text-inherit">ACME</p>
+          <Image
+            width={40}
+            className="mr-4 rounded-full"
+            height={40}
+            src={"/assets/pic.jpg"}
+            alt={""}
+          />
+          <p className="font-bold text-inherit">JESS SHOP</p>
         </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-4" justify="center">
-          {
-            navbarItem.map((item,index : any)=>(
-              <NavbarItem key={index}>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4 lg: " justify="start">
+        <NavbarBrand>
+          <Image
+            width={40}
+            className="mr-4 rounded-full"
+            height={40}
+            src={"/assets/pic.jpg"}
+            alt={""}
+          />
+          <p className="font-bold text-inherit">JESS SHOP</p>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-4 lg: " justify="center">
+        {navbarItem.map((item, index: any) => (
+          <NavbarItem key={index}>
             <Link
               color="foreground"
               href={item.path}
-              className={`${pathname === item.path && "font-bold text-blue-800"}`}
+              className={`${
+                pathname === item.path && "font-bold text-blue-800"
+              }`}
             >
-              {
-                item.title
-              }
+              {item.title}
             </Link>
           </NavbarItem>
-            ))
-          }
-        </NavbarContent>
-        <NavbarContent justify="end">
-          <NavbarItem className="hidden lg:flex">
-            <Link href="/dashboard">Dashboard</Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Button as={Link} color="primary" href="/login" variant="flat">
-              Login
-            </Button>
-          </NavbarItem>
-        </NavbarContent>
-      </Navbar>
-    );
+        ))}
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="/dashboard">Dashboard</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="/login" variant="flat">
+            Login
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu>
+        {navbarItem.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === 2
+                  ? "warning"
+                  : index === navbarItem.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              href={item.path}
+            >
+              {item.title}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+        <NavbarItem className="w-full font">
+          <Link href="/dashboard">Dashboard</Link>
+        </NavbarItem>
+      </NavbarMenu>
+    </Navbar>
+  );
 }
